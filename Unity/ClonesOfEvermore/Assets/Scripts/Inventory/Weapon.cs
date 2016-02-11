b5e9f6623c3da1de3432f476d8822d29b8d8dd54
3dquake <eetu.arvilommi@hotmail.com>
2016-02-11 13:31:03 +0300

    Common * Moved all inventory related code to their respective folder in 'Scripts'
    Item.cs
    * Cloning functionality
    * Added member 'Name'
    * ItemTypes
    
    Added Weapon.cs
    Added Armor.cs
    
    Inventory.cs
    * Inventory functionality. Yet to be tested.

---
+++ Unity/ClonesOfEvermore/Assets/Scripts/Inventory/Weapon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class Weapon : Item
+{
+    public Weapon() : base(1)
+    {
+        Type = ItemType.Weapon;
+    }
+
+    /// <summary>
+    /// Amount of damage we apply to target
+    /// </summary>
+    public int Damage { get; set; }
+
+    public abstract void Attack();
+}
+
